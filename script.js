2fe4d603e8b6a941f2011437b88b3e8cb6e64ccd
dlabanc <78791722+dlabanc@users.noreply.github.com>
2024-02-28 13:36:09 +0100

    NEmtom,megint js méret update

--- script.js
+++ script.js
@@ -190,8 +190,8 @@ document.querySelector(".timer p").addEventListener("click", function () {
 startCountdown();
 
 // Állítsd be a vászon méretét
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth * 0.98;
+canvas.height = window.innerHeight * 0.85;
 
 let drawing = false;
 let cursorColor = "#000";
