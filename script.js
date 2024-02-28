89a50982121883195e85eac494c248fbf9f81147
dlabanc <78791722+dlabanc@users.noreply.github.com>
2024-02-28 13:24:21 +0100

    újTeszt méretre

--- script.js
+++ script.js
@@ -190,8 +190,8 @@ document.querySelector(".timer p").addEventListener("click", function () {
 startCountdown();
 
 // Állítsd be a vászon méretét
-//canvas.width = window.innerWidth * 0.95;
-//canvas.height = window.innerHeight * 0.95;
+canvas.width = window.innerWidth * 0.98;
+canvas.height = window.innerHeight * 0.75;
 
 let drawing = false;
 let cursorColor = "#000";
